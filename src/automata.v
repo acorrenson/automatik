4bb8e7d1880dd7fdd499892a7e9e1818a53b2fa9
Arthur Correnson <arthur.correnson@gmail.com>
2020-09-21 17:18:40 +0200

    Equivalence between semantics

--- src/automata.v
+++ src/automata.v
@@ -1,4 +1,8 @@
-Require Import Coq.Lists.List Init.Nat Coq.Program.Equality Lia. 
+Require Import 
+  Lists.List
+  Init.Nat
+  Lia
+  Program.Equality.
 Import ListNotations.
 
 Set Implicit Arguments.
@@ -6,119 +10,69 @@ Set Implicit Arguments.
 Record dfa (S A : Type) := DFA {
   initial_state : S;
   is_final : S -> bool;
-  next : S -> A -> option S
+  next : S -> A -> S
 }.
 
-Definition apply_trans S A (m : dfa S A) s a :=
-  match s with
-  | None    => None
-  | Some s' => next m s' a
-  end.
+Definition sem_denot_dfa (S A : Type) (w : list A) (m : dfa S A) : bool :=
+  is_final m (fold_left (next m) w (initial_state m)).
 
-Definition check S A (m : dfa S A) s :=
-  match s with
-  | None    => false
-  | Some s' => is_final m s'
-  end.
+(* Inductive sem_op_dfa (S A : Type) (m : dfa S A) : S -> A -> S -> Prop :=
+  | step : forall q c,
+    sem_op_dfa m q c (next m q c). *)
 
-Inductive transition (S A : Type) (m : dfa S A) : S -> A -> S -> Prop :=
-  | step :
-    forall s1 c s2,
-    next m s1 c = Some (s2) -> transition m s1 c s2.
-
-Inductive transitions (S A : Type) (m : dfa S A) : S -> list A -> S -> Prop :=
-  | one_step :
-    forall s1 c s2,
-    transition m s1 c s2 ->
-    transitions m s1 [c] s2
-  | no_step :
-    forall s,
-    transitions m s [] s
-  | many_steps :
-    forall s1 c w s2 s3,
-    transition m s1 c s2 ->
-    transitions m s2 w s3 ->
-    transitions m s1 (c::w) s3.
+Inductive sem_op_dfa (S A : Type) (m : dfa S A) : S -> list A -> S -> Prop :=
+  | no_step   : forall q,
+    sem_op_dfa m q [] q
+  
+  | one_step  : forall q c,
+    sem_op_dfa m q [c] (next m q c)
+  
+  | steps     : forall q q' c w,
+    sem_op_dfa m (next m q c) w q' ->
+    sem_op_dfa m q (c::w) q'.
 
 
-Definition run_dfa S A (m : dfa S A) (l : list A) : bool :=
-  let i := Some (initial_state m) in
-  check m (fold_left (apply_trans m) l i).
+Notation "[| m |] w " := (sem_denot_dfa w m) (at level 80).
+Notation "[| m |] ( w , q )" := (fold_left (next m) w q) (at level 80).
+Notation "( m , q1 , w ) --> q2" := (sem_op_dfa m q1 w q2) (at level 80).
 
-Theorem transitions_run_dfa:
-  forall S A (m : dfa S A) w,
-  run_dfa m w = true <-> exists f, is_final m f = true /\ transitions m (initial_state m) w f. 
+
+Lemma fold_trans :
+  forall S A (m : dfa S A) w q1 q2,
+  fold_left (next m) w q1 = q2
+  <->
+  sem_op_dfa m q1 w q2.
 Proof.
-  intros S A m w.
-  induction w; split; intro.
-  - unfold run_dfa in *; simpl in *.
-    exists (initial_state m).
-    split; [assumption | apply no_step].
-  - unfold run_dfa in *; simpl in *.
-    elim H. intros x [H1 H2].
-    inversion H2. subst.
-    assumption.
-  - unfold run_dfa in H.
-    destruct (fold_left (apply_trans m) (a :: w) (Some (initial_state m))).
-    + exists s.
-      split.
-      * inversion H; reflexivity.
-      * eapply many_steps.
-        ** apply.
-
-
-
-
-
+  intros. split.
+  - dependent induction w; intros H.
+    + elim H. apply no_step.
+    + apply steps.
+      apply IHw.
+      assumption.
+  - dependent induction w; intros H.
+    + inversion H; subst; reflexivity.
+    + inversion H; subst.
+      ++ reflexivity.
+      ++ simpl. apply IHw. assumption.
 Qed.
 
-Inductive binary := I | O.
 
-Inductive states := S0 | S1 | S2.
-
-Definition m3 : dfa states binary := {|
-  initial_state := S0;
-  is_final s := match s with S0 => true | _ => false end;
-  next s x :=
-    match s, x with
-    | S0, O => Some S0
-    | S0, I => Some S1
-    | S1, O => Some S2
-    | S1, I => Some S0
-    | S2, O => Some S1
-    | S2, I => Some S1
-    end;
-|}.
-
-Fixpoint base_2 (w : list binary) : nat :=
-  match w with
-  | [] => 0
-  | I::x => (pow 2 (length x)) * base_2 x
-  | O::x => base_2 x
-  end.
-
-Definition divide a b := exists k, b = a*k.
-
-
-Theorem m3_correct1 :
-  forall w, run_dfa m3 w = true -> divide 3 (base_2 w).
+Theorem sem_equiv :
+  forall S A (m : dfa S A) w,
+  sem_denot_dfa w m = true
+  <->
+  exists k, is_final m k = true /\ sem_op_dfa m (initial_state m) w k.
 Proof.
-  induction w; intro H.
-  - exists 0; reflexivity.
-  - destruct a; simpl.
-    case_eq (run_dfa m3 w); intro.
-    + elim (IHw H0). intros.
-      exists (x * 2 ^ length w). lia.
-    + unfol
-    
-    
-  
-  - intros [k Hw].
-    induction w.
-    + reflexivity.
-    + destruct a.
-      unfold run_dfa in *.
-      simpl.
-  -
-
-
+  intros S A m w. 
+  eelim (fold_trans m w).
+  intros. split.
+  - intro.
+    unfold sem_denot_dfa in H1.
+    exists ([|m|](w, initial_state m)).
+    split; [assumption | apply H; reflexivity].
+  - intros [x [H1 H2]].
+    apply fold_trans in H2.
+    unfold sem_denot_dfa; simpl.
+    subst.
+    assumption.
+Qed.
